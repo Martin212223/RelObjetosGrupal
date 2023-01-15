8fd02e5aaf4c1bebd494749bed0cc9704934ea0a
Martín Quiroga <quiroga.martin.augusto@gmail.com>
2023-01-15 02:18:49 -0300

    terminado

---
+++ WindowsRelaciones/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsRelaciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listProducts = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(211, 247);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Listar productos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.MostrarProductos);
+            // 
+            // listProducts
+            // 
+            this.listProducts.FormattingEnabled = true;
+            this.listProducts.Location = new System.Drawing.Point(25, 24);
+            this.listProducts.Name = "listProducts";
+            this.listProducts.Size = new System.Drawing.Size(497, 199);
+            this.listProducts.TabIndex = 2;
+            this.listProducts.SelectedIndexChanged += new System.EventHandler(this.listProducts_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 294);
+            this.Controls.Add(this.listProducts);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listProducts;
+    }
+}
+
---
+++ WindowsRelaciones/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibFactura.Entidad;
+
+namespace WindowsRelaciones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MostrarProductos(object sender, EventArgs e)
+        {
+            //foreach (var item in Lista.Listar())
+            //{
+            //listProducts.Items.Add(item.Categoria.Nombre);
+            //}
+
+            Categoria categoria1 = new Categoria("Ropa");
+            Categoria categoria2 = new Categoria("Calzado");
+
+            //Console.WriteLine(categoria1.Nombre);
+
+            Producto producto1 = new Producto("Pantalón", 6000.50)
+            {
+                Categoria = categoria1
+            };
+            Producto producto2 = new Producto("Zapatillas", 12000.50) 
+            {
+                Categoria = categoria2
+            };
+
+            List<Producto> listaProductos = new List<Producto>();
+
+            listaProductos.Add(producto1);
+            listaProductos.Add(producto2);
+
+            foreach (Producto producto in listaProductos)
+            {
+                listProducts.Items.Add(producto);
+            }
+
+
+        }
+
+        private void gridProductos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
